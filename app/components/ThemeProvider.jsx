889f5659f3ea265549fb1d1dad4caa56554dcadc
mas3edo <ahmedmasshoud929@gmail.com>
2026-05-09 22:58:15 +0300

    Fix: Restore Dark/Light mode theme switching functionality

--- app/components/ThemeProvider.jsx
+++ app/components/ThemeProvider.jsx
@@ -4,7 +4,13 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+    <NextThemesProvider 
+      attribute="class" 
+      defaultTheme="dark" 
+      enableSystem
+      storageKey="theme"
+      forcedTheme={undefined}
+    >
       {children}
     </NextThemesProvider>
   );
